dd8ee31eb25927ae1733d30443508eb4ac7f69d9
Ahmed Haque <afhaque@rice.edu>
2016-11-17 16:32:19 -0500

    Fixed example-1

--- Example-1/server.js
+++ Example-1/server.js
@@ -1,41 +1,16 @@
-// Dependencies
-// =============================================================
-var express = require("express");
-var bodyParser = require("body-parser");
-var path = require("path");
-var mysql = require("mysql");
-var passport = require("passport")
-var Strategy = require("passport-facebook").Strategy
+var express = require('express');
+var passport = require('passport');
+var Strategy = require('passport-facebook').Strategy;
+var path = require('path');
 
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = process.env.PORT || 3000;
 
-// Sets up the Express app to handle data parsing 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-
-// Setup MySQL Connection
-var connection = mysql.createConnection({
-  port: 3306,
-  host: 'localhost',
-  user: 'root',
-  password: 'CodingRocks!',
-  database: 'ahmedsdb'
-});
-
-connection.connect(function (err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
-  console.log('connected as id ' + connection.threadId);
-});
-
-// Sets up the Passport Authentication 
+// Configure the Facebook strategy for use by Passport.
+//
+// OAuth 2.0-based strategies require a `verify` function which receives the
+// credential (`accessToken`) for accessing the Facebook API on the user's
+// behalf, along with the user's profile.  The function must invoke `cb`
+// with a user object, which will be set at `req.user` in route handlers after
+// authentication.
 passport.use(new Strategy({
     clientID: process.env.CLIENT_ID || "1826103597601691",
     clientSecret: process.env.CLIENT_SECRET || "1c5d8736244d4ecadc89fe7c0384eff0",
@@ -50,6 +25,16 @@ passport.use(new Strategy({
     return cb(null, profile);
   }));
 
+
+// Configure Passport authenticated session persistence.
+//
+// In order to restore authentication state across HTTP requests, Passport needs
+// to serialize users into and deserialize users out of the session.  In a
+// production-quality application, this would typically be as simple as
+// supplying the user ID when serializing, and querying the user record by ID
+// from the database when deserializing.  However, due to the fact that this
+// example does not have a database, the complete Facebook profile is serialized
+// and deserialized.
 passport.serializeUser(function(user, cb) {
   cb(null, user);
 });
@@ -58,11 +43,26 @@ passport.deserializeUser(function(obj, cb) {
   cb(null, obj);
 });
 
-app.use(passport.initialize());
-app.use(passport.session());
+
+// Create a new Express application.
+var app = express();
+
+// Configure view engine to render EJS templates.
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+// Use application-level middleware for common functionality, including
+// logging, parsing, and session handling.
+app.use(require('morgan')('combined'));
 app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
 app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 
 // Routes
@@ -94,6 +94,7 @@ app.get("/inbox",
 
   function(req, res) {
 
+      console.log("HELLO");
       console.log(req.user)
       res.json(req.user);
 
@@ -103,6 +104,7 @@ app.get("/inbox",
 
 // Starts the server to begin listening
 // =============================================================
+var PORT = process.env.PORT || 3002;
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
